4587cfae8d39785d589606fa99801701984509ec
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2026-09-16 20:14:03 +0700

    feat: extend models

--- weather_dash/models.py
+++ weather_dash/models.py
@@ -6,3 +6,15 @@ class WilayahCreate(BaseModel):
     max_rainfall_mm: float
     max_wind_speed_kph: float
 
+class WilayahResponse(BaseModel):
+    id: int
+    nama_wilayah: str
+    max_rainfall_mm: float
+    max_wind_speed_kph: float
+
+class WeatherCreate(BaseModel):
+    wilayah_id: int
+    rainfall_mm: float
+    wind_speed_kph: float
+    temperature_c: float
+
